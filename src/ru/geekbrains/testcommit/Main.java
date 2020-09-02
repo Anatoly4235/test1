78d585c19dd1e0f591c7d7fe8f8fee8cbb2dd7c5
Anatoly <kosov4235@yandex.ru>
2020-09-02 12:18:01 +0300

    Первый сохраненный коммит

--- src/ru/geekbrains/testcommit/Main.java
+++ src/ru/geekbrains/testcommit/Main.java
@@ -1,8 +1,10 @@
 package ru.geekbrains.testcommit;
 
+import org.w3c.dom.ls.LSOutput;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        System.out.println("Проверяем коммит");
     }
 }
